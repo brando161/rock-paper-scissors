b721f955b0b7192060a0b2db9f48bf2599bc75ef
Brandon Ortega <114270169+brando161@users.noreply.github.com>
2023-10-14 22:41:46 -0400

    Create a function that plays a round
    I created another function that plays a single round of rock, paper, scissors. The function accepts two parameters (playerSelection, computerSelection). I also declared those parameters as variables. I chose my own input for playerSelection and I assigned gerComputerChoice with computerSelection.

--- script.js
+++ script.js
@@ -15,4 +15,30 @@ const getComputerChoice = () => {
     };
 }
 
-console.log(getComputerChoice())
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return "It's a tie!"
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You lose! Paper beats Rock."
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors."
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock."
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        return "It's a tie!"
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beats Paper."
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors."
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beats Paper."
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "It's a tie!"
+    } else {
+        return "Error"
+    }
+}
+
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
