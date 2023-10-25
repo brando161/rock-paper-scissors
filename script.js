c2fcc712e7814bea95a2f449dba1176101c132a6
Brandon Ortega <114270169+brando161@users.noreply.github.com>
2023-10-25 15:44:03 -0400

    Fixed issue with function
    Every time I clicked on a button to declare my choice, the computer would only generate one choice during the entire game. Turns out that computerSelection is only called once and will constanly give out one choice. To fix this, I declared computerSelection again in all three addEventListener()'s so the computer would generate different choices each time I made my own.

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ const scissors = document.querySelector("#scissors");
 const computerChoice = ["ROCK", "PAPER", "SCISSORS"];
 
 const getComputerChoice = () => {
-    const randomChoice = Math.floor(Math.random() * 3);
-    return computerChoice[randomChoice];
+    let randomChoice = Math.floor(Math.random() * 3);
+    switch (randomChoice) {
+        case 0:
+            return "ROCK";
+        break;
+
+        case 1:
+            return "PAPER";
+        break;
+
+        case 2:
+            return "SCISSORS";
+        break;
+    };
 }
 
-const playRound = (playerSelection) => {
-    const computerSelection = getComputerChoice();
-    
+const computerSelection = getComputerChoice();
+
+const playRound = (playerSelection, computerSelection) => {
     const win = 
         (playerSelection === "ROCK" && computerSelection === "SCISSORS") || 
         (playerSelection === "PAPER" && computerSelection === "ROCK") ||
@@ -34,30 +46,33 @@ const playRound = (playerSelection) => {
 }
 
 rock.addEventListener("click", function() {
-    const result = playRound("ROCK");
-    console.log(result);
-    finalScore();
+    const computerSelection = getComputerChoice();
+    playRound("ROCK", computerSelection);
+    console.log(`You chose ROCK and the computer chose ${computerSelection}.`);
+    // finalScore();
 });
 paper.addEventListener("click", function() {
-    const result = playRound("PAPER");
-    console.log(result);
-    finalScore();
+    const computerSelection = getComputerChoice();
+    playRound("PAPER", computerSelection);
+    console.log(`You chose PAPER and the computer chose ${computerSelection}.`);
+    // finalScore();
 });
 scissors.addEventListener("click", function() {
-    const result = playRound("SCISSORS");
-    console.log(result);
-    finalScore();
+    const computerSelection = getComputerChoice();
+    playRound("SCISSORS", computerSelection);
+    console.log(`You chose SCISSORS and the computer chose ${computerSelection}.`);
+    // finalScore();
 });
 
-const finalScore = () => {
-    console.log(`Your Score: ${playerScore}`);
-    console.log(`Computer's Score: ${computerScore}`);
+// const finalScore = () => {
+//     console.log(`Your Score: ${playerScore}`);
+//     console.log(`Computer's Score: ${computerScore}`);
 
-    if(playerScore > computerScore) {
-        alert(`Congratulations, you won the game! Your score was ${playerScore} and the computer's score was ${computerScore}`);
-    } else if(playerScore === computerScore) {
-        alert(`It's a tied game! Both scores were ${playerScore}`);
-    } else {
-        alert(`You lost the game! Your score was ${playerScore} and the computer's score was ${computerScore}`);
-    }
-};
+//     if(playerScore > computerScore) {
+//         alert(`Congratulations, you won the game! Your score was ${playerScore} and the computer's score was ${computerScore}`);
+//     } else if(playerScore === computerScore) {
+//         alert(`It's a tied game! Both scores were ${playerScore}`);
+//     } else {
+//         alert(`You lost the game! Your score was ${playerScore} and the computer's score was ${computerScore}`);
+//     }
+// };
