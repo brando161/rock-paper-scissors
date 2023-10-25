5355081a0d612d289a5540ffebdb7a9459281830
Brandon Ortega <114270169+brando161@users.noreply.github.com>
2023-10-24 20:20:35 -0400

    Made UI buttons interactive
    First, I made my code for script.js simpler. I assigned each button element as their respective variable. Then I created a addEventListener() where the player's selection would be declared depending on the button they clicked.

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
 let playerScore = 0;
 let computerScore = 0;
 
-const game = () => {
-    for(i = 1; i < 6; i++) {
-        const getComputerChoice = () => {
-            let randomNumber = Math.floor(Math.random() *3);
-            switch(randomNumber) {
-            case 0:
-                return "Rock";
-            break;
-        
-            case 1:
-                return "Paper";
-            break;
-        
-            case 2:
-                return "Scissors";
-            break;
-            };
-        }
-    
-        const playRound = (playerSelection, computerSelection) => {
-            const win = (playerSelection === "ROCK" && computerSelection === "SCISSORS") || 
-            (playerSelection === "PAPER" && computerSelection === "ROCK") ||
-            (playerSelection === "SCISSORS" && computerSelection === "PAPER");
-            if(playerSelection === computerSelection) {
-                return "It's a tie!";
-            }
-            if(win) {
-                playerScore++;
-                return "You win!";
-            } else {
-                computerScore++;
-                return "You lose!";
-            }
-        }
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-        // const playerChoice = prompt(`Round ${i}: Choose Rock, Paper, or Scissors.`, "");
-        const playerSelection = playerChoice.toUpperCase();
-        const computerSelection = getComputerChoice().toUpperCase();
+const computerChoice = ["ROCK", "PAPER", "SCISSORS"];
+
+const getComputerChoice = () => {
+    const randomChoice = Math.floor(Math.random() * 3);
+    return computerChoice[randomChoice];
+}
+
+const playRound = (playerSelection) => {
+    const computerSelection = getComputerChoice();
     
-        console.log(playRound(playerSelection, computerSelection));
+    const win = 
+        (playerSelection === "ROCK" && computerSelection === "SCISSORS") || 
+        (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER");
     
-        console.log(`You chose ${playerSelection}`);
-        console.log(`Computer chose ${computerSelection}`);
+    if(playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+
+    if(win) {
+        playerScore++;
+        return "You win!";
+    } else {
+        computerScore++;
+        return "You lose!";
     }
 }
 
-game();
+rock.addEventListener("click", function() {
+    const result = playRound("ROCK");
+    console.log(result);
+    finalScore();
+});
+paper.addEventListener("click", function() {
+    const result = playRound("PAPER");
+    console.log(result);
+    finalScore();
+});
+scissors.addEventListener("click", function() {
+    const result = playRound("SCISSORS");
+    console.log(result);
+    finalScore();
+});
 
 const finalScore = () => {
+    console.log(`Your Score: ${playerScore}`);
+    console.log(`Computer's Score: ${computerScore}`);
+
     if(playerScore > computerScore) {
         alert(`Congratulations, you won the game! Your score was ${playerScore} and the computer's score was ${computerScore}`);
     } else if(playerScore === computerScore) {
@@ -57,9 +60,4 @@ const finalScore = () => {
     } else {
         alert(`You lost the game! Your score was ${playerScore} and the computer's score was ${computerScore}`);
     }
-}
-
-finalScore();
-
-console.log(`Your Score: ${playerScore}`);
-console.log(`Computer's Score: ${computerScore}`);
+};
