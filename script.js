3376d8be629708525f6d6e6f941374678c23ccff
Brandon Ortega <114270169+brando161@users.noreply.github.com>
2023-10-27 15:53:36 -0400

    Updated result display and score count
    The results and score count will now be displayed on the UI.

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
-const computerChoice = ["ROCK", "PAPER", "SCISSORS"];
+const resultText = document.getElementById("resultText");
+const score1 = document.getElementById("score1");
+const score2 = document.getElementById("score2");
 
 const getComputerChoice = () => {
     let randomChoice = Math.floor(Math.random() * 3);
@@ -48,28 +50,28 @@ const playRound = (playerSelection, computerSelection) => {
 rock.addEventListener("click", function() {
     const computerSelection = getComputerChoice();
     const result = playRound("ROCK", computerSelection);
-    console.log(result);
-    console.log(`You chose ROCK and the computer chose ${computerSelection}.`);
+    
+    resultText.textContent = `${result} You chose ROCK and the computer chose ${computerSelection}.`;
     finalScore();
 });
 paper.addEventListener("click", function() {
     const computerSelection = getComputerChoice();
     const result = playRound("PAPER", computerSelection);
-    console.log(result);
-    console.log(`You chose PAPER and the computer chose ${computerSelection}.`);
+    
+    resultText.textContent = `${result} You chose PAPER and the computer chose ${computerSelection}.`;
     finalScore();
 });
 scissors.addEventListener("click", function() {
     const computerSelection = getComputerChoice();
     const result = playRound("SCISSORS", computerSelection);
-    console.log(result);
-    console.log(`You chose SCISSORS and the computer chose ${computerSelection}.`);
+    
+    resultText.textContent = `${result} You chose SCISSORS and the computer chose ${computerSelection}.`;
     finalScore();
 });
 
 const finalScore = () => {
-    console.log(`Your Score: ${playerScore}`);
-    console.log(`Computer's Score: ${computerScore}`);
+    score1.textContent = `Your Score: ${playerScore}`;
+    score2.textContent = `Computer's Score: ${computerScore}`;
 
     // if(playerScore > computerScore) {
     //     alert(`Congratulations, you won the game! Your score was ${playerScore} and the computer's score was ${computerScore}`);
